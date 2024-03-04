fb1b6b45ba0cc16e022f48893591b8c4134e6659
User <Willspri@gmail.com>
2024-03-05 12:17:12 +1300

    complete

--- MovieEdit/MovieEdit.py
+++ MovieEdit/MovieEdit.py
@@ -1,5 +1,9 @@
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+from moviepy.editor import VideoFileClip, concatenate_videoclips, vfx
 
 clip1 = VideoFileClip("filename").subclip(10, 20) #take from second 10 to second 20
-clip2 = VideoFileClip("filename2").subclip(10, 20)
+clip2 = VideoFileClip("filename2").subclip(10, 20).fx(vfx.fadein, 1).fx(vfx.fadeout, 1)
 clip3 = VideoFileClip("filename3").subclip(20, 30)
+clip4 = VideoFileClip("filename4").subclip(10, 20)
+
+combined = concatenate_videoclips([clip1, clip2, clip3])
+combined.write_videofile("combined.mp4")
